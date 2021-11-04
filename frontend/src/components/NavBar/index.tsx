52b95cceb3ab0ddfdfd4098b5733c1723f095035
Idilene Brito <idilenebrito98.ib@gmail.com>
2021-11-03 21:45:26 -0300

    adicionando alt no NavBar

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -6,7 +6,7 @@ const NavBar = () => {
       <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
         <div className="container">
           <nav className="my-2 my-md-0 mr-md-3">
-            <img src={ImgDsDark} width="120" />
+            <img src={ImgDsDark}  alt="logo-devsuperior" width="120" />
           </nav>
         </div>
       </div>
