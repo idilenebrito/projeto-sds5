245e848f51785972fd46da0f0bba3d4cb5ca05cc
Idilene Brito <idilenebrito98.ib@gmail.com>
2021-11-07 19:24:24 -0300

    DonutChart integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,6 +1,57 @@
+import axios from 'axios';
+import { round } from 'components/utils/format';
+import { Base_URL } from 'components/utils/requests';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
+import { SaleSuccess } from 'types/sale';
+
+type SeriesData = {
+    name: string;
+    data: number[];
+}
+
+type ChartData = {
+    labels: {
+        categories: string[];
+    };
+    series: SeriesData[];
+}
 
 const BarChart = () =>{
+
+    const [chartData, setChartData] = useState<ChartData>({
+        labels: {
+            categories: []
+        },
+        series: [
+            {
+                name: "",
+                data: []                   
+            }
+        ]
+    });
+
+    useEffect(()=> {
+        axios.get(`${Base_URL}/sales/success-by-seller`)
+        .then(response => {
+            const data = response.data as SaleSuccess[];
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => round(100.0 * x.deals / x.visited , 1));
+    
+            setChartData ({
+                labels: {
+                    categories: myLabels
+                },
+                series: [
+                    {
+                        name: "% de Sucesso",
+                        data: mySeries                   
+                    }
+                ]
+            });
+        });
+    }, []);
+
 const options = {
     plotOptions: {
         bar: {
@@ -9,23 +60,10 @@ const options = {
     },
 };
 
-
-const mockData = {
-    labels: {
-        categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    },
-    series: [
-        {
-            name: "% Sucesso",
-            data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
-        }
-    ]
-};
-
 return (
     <Chart 
-        options={{ ...options, xaxis: mockData.labels}}
-        series={mockData.series}
+        options={{ ...options, xaxis: chartData.labels}}
+        series={chartData.series}
         type="bar"
         heigth="240"
     />
