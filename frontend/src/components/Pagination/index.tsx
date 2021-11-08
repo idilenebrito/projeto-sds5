b31c566bcdcad225f4125ad8efe1b4a07340d3d7
Idilene Brito <idilenebrito98.ib@gmail.com>
2021-11-07 22:07:15 -0300

    imagens do projeto

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const Pagination = ({ page, onPageChange } : Props) => {
   return (
-    <div className=" center">
+    <div className="center">
       <nav>
         <ul className="pagination">
           <li className={`page-item ${page.first ? "disabled" : ""} `}>
