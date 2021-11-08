d52517f19c91e3a32489f2ea5c80d95821b98e4d
Idilene Brito <idilenebrito98.ib@gmail.com>
2021-11-07 21:54:38 -0300

    Env var

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const Pagination = ({ page, onPageChange } : Props) => {
   return (
-    <div className="row d-flex justify-content-center">
+    <div className=" center">
       <nav>
         <ul className="pagination">
           <li className={`page-item ${page.first ? "disabled" : ""} `}>
